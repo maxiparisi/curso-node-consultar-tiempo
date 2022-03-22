b2b453457f9d7eba378c6767199e3c9686ae7abf
Maximiliano Parisi <maximiliano.parisi@gire.com>
2022-03-22 20:51:18 -0300

    creo una instancia de axios, cosa q sea un poco mas parametrizable

--- models/busqueda.js
+++ models/busqueda.js
@@ -9,9 +9,24 @@ class Busqueda {
         //TODO_: leer db
     }
 
+    get mapboxParams() {
+        return {
+            'access_token': 'pk.eyJ1IjoibXBhcmlzaSIsImEiOiJjbDB5d3BtZGYwbDV2M2RvMGZzOTZqMW9lIn0.rvaWf1wCXWcVDTBbjHqAJA',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async buscarLugar(lugar = ''){
         //peticion http
-        const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/'+lugar+'.json?limit=5&language=es&access_token=pk.eyJ1IjoibXBhcmlzaSIsImEiOiJjbDB5d3BtZGYwbDV2M2RvMGZzOTZqMW9lIn0.rvaWf1wCXWcVDTBbjHqAJA')
+        
+        const instance = axios.create({
+            baseURL: 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+lugar+'.json',
+            timeout: 3000,
+            params: this.mapboxParams
+          });
+
+        const resp = await instance.get();
         console.log(resp.data);
 
         return []; // retornar lugares q coincidan con la busqueda
